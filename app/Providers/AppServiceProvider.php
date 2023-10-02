6d6eb9770772594df9f0c022c358ca9b61cf8bc5
DESKTOP-49UPDS3\Salgado <sebastesb1986@gmail.com>
2023-10-02 12:04:29 -0500

    Base project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (!App::environment([
+            'local',
+            'testing',
+        ])) {
+            URL::forceScheme('https');
+        }
+        
         Paginator::useBootstrap();
 
     }
