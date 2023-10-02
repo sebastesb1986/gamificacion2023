d9718243556570e47fa7e12d4498cb9fa54b688e
DESKTOP-49UPDS3\Salgado <sebastesb1986@gmail.com>
2023-10-02 13:14:55 -0500

    Base project

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -4,7 +4,7 @@
 <head>
 
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
